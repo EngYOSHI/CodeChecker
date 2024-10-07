76a1d011d7be607cf3c2b9b3407142be1c93cdfb
EngYOSHI <243426029@ccmailg.meijo-u.ac.jp>
2024-10-08 02:22:57 +0900

    Fix: 最新のソースコードを識別する方法が誤っていたので修正
    ・出力からソースコード名の列を削除

--- main.py
+++ main.py
@@ -50,7 +50,6 @@ def writeuntouched():
 def writexl(res):
 	#[{student:str,
 	#  result:[{task:str,
-	#           src:str|None,
 	#           compile:{result:bool, reason:None|str, stdout:str|None},
 	#           run:[{result:bool|None, reason:None|str, output:str|None, ratio:float|None} | None]
 	#         }]
@@ -60,36 +59,34 @@ def writexl(res):
 	wb.properties.creator = 'CodeChecker'
 	wb.properties.lastModifiedBy = 'CodeChecker'
 	ws = wb["Sheet"]
-	ws.append(["学籍番号", "課題番号", "ソースコード名", "ｺﾝﾊﾟｲﾙ結果", "コンパイル備考", "コンパイルログ", "ﾃｽﾄｹｰｽ", "テスト結果", "テスト結果備考", "ﾃｽﾄｹｰｽ一致率", "標準出力"])
-	for c in list("ABCEFIK"):
+	ws.append(["学籍番号", "課題番号", "ｺﾝﾊﾟｲﾙ結果", "コンパイル備考", "コンパイルログ", "ﾃｽﾄｹｰｽ", "テスト結果", "テスト結果備考", "ﾃｽﾄｹｰｽ一致率", "標準出力"])
+	for c in list("ABDEHJ"):
 		ws.column_dimensions[c].width = 20
-	ws.column_dimensions["D"].width = 10
-	ws.column_dimensions["G"].width = 7
-	ws.column_dimensions["H"].width = 11
-	ws.column_dimensions["J"].width = 13
+	ws.column_dimensions["C"].width = 10
+	ws.column_dimensions["F"].width = 7
+	ws.column_dimensions["G"].width = 11
+	ws.column_dimensions["I"].width = 13
 	ws.freeze_panes = "A2" #先頭行を固定
 	row = 2
 	for res_student in res:
 		ws.cell(row=row, column=1).value = res_student["student"]
 		for task in res_student["result"]:
 			ws.cell(row=row, column=2).value = task["task"]
-			if task["src"] is not None:
-				ws.cell(row=row, column=3).value = task["src"]
-			ws.cell(row=row, column=4).value = valconv(task["compile"]["result"], bool, t = "OK", f = "NG")
-			cellfill(ws.cell(row=row, column=4), [("OK", "00b050"),("NG", "e09694")])
-			ws.cell(row=row, column=5).value = valconv(task["compile"]["reason"], str, none="")
-			ws.cell(row=row, column=6).value = valconv(task["compile"]["stdout"], str, none="")
+			ws.cell(row=row, column=3).value = valconv(task["compile"]["result"], bool, t = "OK", f = "NG")
+			cellfill(ws.cell(row=row, column=3), [("OK", "00b050"),("NG", "e09694")])
+			ws.cell(row=row, column=4).value = valconv(task["compile"]["reason"], str, none="")
+			ws.cell(row=row, column=5).value = valconv(task["compile"]["stdout"], str, none="")
 			if task["run"] is None:
-				ws.cell(row=row, column=7).value = "　" #コンパイルログが右のセルにはみ出さないように
+				ws.cell(row=row, column=6).value = "　" #コンパイルログが右のセルにはみ出さないように
 				row += 1
 				continue
 			for i, run in enumerate(task["run"]):
-				ws.cell(row=row, column=7).value = task["task"] + "_" + str(i + 1)
-				ws.cell(row=row, column=8).value = valconv(run["result"], bool, t="OK", f="NG", none="SKIP")
-				cellfill(ws.cell(row=row, column=8), [("OK", "00b050"),("NG", "e09694"),("SKIP", "c5d9f1")])
-				ws.cell(row=row, column=9).value = valconv(run["reason"], str, none="")
-				ws.cell(row=row, column=10).value = valconv(run["ratio"], float, none="")
-				ws.cell(row=row, column=11).value = valconv(run["output"], str, none="")
+				ws.cell(row=row, column=6).value = task["task"] + "_" + str(i + 1)
+				ws.cell(row=row, column=7).value = valconv(run["result"], bool, t="OK", f="NG", none="SKIP")
+				cellfill(ws.cell(row=row, column=7), [("OK", "00b050"),("NG", "e09694"),("SKIP", "c5d9f1")])
+				ws.cell(row=row, column=8).value = valconv(run["reason"], str, none="")
+				ws.cell(row=row, column=9).value = valconv(run["ratio"], float, none="")
+				ws.cell(row=row, column=10).value = valconv(run["output"], str, none="")
 				row += 1
 	path = os.path.join(RESULT_PATH, "result.xlsx")
 	while True:
@@ -134,15 +131,15 @@ def eval_loop(students, tasks):
 
 
 def eval(student, task):
-	r = {"task":task["name"], "src":None, "compile":None, "run":None}
+	r = {"task":task["name"], "compile":None, "run":None}
 	exe = f"{student}_{task["name"]}"
 	debug("\n")
 	debug(exe, "eval")
 	debug("----------------------")
-	r["src"] = findLatestSrc(student, task["name"])
-	debug(r["src"], "srcfile")
+	src = findLatestSrc(student, task["name"])
+	debug(src, "srcfile")
 	#コンパイル
-	r["compile"] = compile(r["src"], exe)
+	r["compile"] = compile(src, exe)
 	debug(r["compile"], "compile")
 	if r["compile"]["result"]:
 		#実行
@@ -152,8 +149,8 @@ def eval(student, task):
 
 
 def findLatestSrc(student, taskname):
-	#命名条件に合致するファイルが一つもなければNone
-	#命名条件に合致するファイルがあれば，評価対象のファイル名
+	#命名条件に合致するファイルが一つもなければNoneを返す
+	#命名条件に合致するファイルがあれば，評価対象のファイル名を返す
 	global srclist
 	r = student + "_" + taskname + r"(\([1-9][0-9]*\))?" + ".c"
 	src = [x for x in srclist if re.match(r, x)]
@@ -164,17 +161,8 @@ def findLatestSrc(student, taskname):
 	for s in src:
 		srclist.remove(s)
 	#チェックすべきソースファイルを識別
-	if len(src) == 1:
-		#候補が一つしかないときは，それがチェックすべきソースファイル
-		return src[0]
-	else:
-		maxnum = 0
-		#複数候補がある場合，必ずファイル名に丸括弧が含まれている
-		for i in range(len(src)):
-			if "(" in src[i]:
-				num = int(re.search(r"(\([1-9][0-9]*\))", src[i]).group()[1:-1])
-				maxnum = max(num, maxnum)
-		return f"{student}_{taskname}({maxnum}).c"
+	#最新の提出物は，拡張子の前に番号が「付かない」，ピュアなファイル名のもの
+	return f"{student}_{taskname}.c"
 
 
 def compile(src, exe):
@@ -243,8 +231,6 @@ def printScore(s, res_student, progress):
 	output = "Student No.: " + s + "   " + progress
 	for r in res_student:
 		output += "\n\tTask: " + r["task"]
-		if r["src"] is not None:
-			output += "\n\t\tSourceFile: " + r["src"]
 		output += "\n\t\tCompile: " + bool2str(r["compile"]["result"], "OK", "NG")
 		if r["compile"]["result"]:
 			correct = 0
